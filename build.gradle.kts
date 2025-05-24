27f6a88c6ed98b2aa47aec1d7ff81dbadd5a2c78
OJaes <ojg1117@gmail.com>
2025-05-24 14:10:17 +0900

    settings of error

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,13 +29,28 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// database
 	implementation("org.flywaydb:flyway-core")
-	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("com.h2database:h2")
+
+	//swagger
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
+
+	//jwt
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+	compileOnly("org.projectlombok:lombok")
+
+
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
