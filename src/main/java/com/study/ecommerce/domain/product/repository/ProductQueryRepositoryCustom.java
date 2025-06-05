53ba61c5d49c021047e8d065f8760289bdea16fe
OJaes <ojg1117@gmail.com>
2025-06-06 03:24:34 +0900

    0606 과제 subquery 첨부

--- src/main/java/com/study/ecommerce/domain/product/repository/ProductQueryRepositoryCustom.java
+++ src/main/java/com/study/ecommerce/domain/product/repository/ProductQueryRepositoryCustom.java
@@ -4,8 +4,10 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.study.ecommerce.domain.category.entity.QCategory;
 import com.study.ecommerce.domain.product.dto.req.ProductSearchCondition;
 import com.study.ecommerce.domain.product.dto.resp.ProductSummaryDto;
 import com.study.ecommerce.domain.product.entity.Product;
@@ -30,34 +32,48 @@ public class ProductQueryRepositoryCustom implements ProductQueryRepository{
             Pageable pageable
     ) {
 
+        // sql Select product as product랑 같다보 보면 될 듯
+        // alias 주고 가져오는 부분?
         QProduct product = QProduct.product;
+        QCategory category = QCategory.category;
 
+        // sql where 부분?
         // 동적 쿼리를 생성하기 위한 조건
         BooleanBuilder builder = new BooleanBuilder();
 
+        // 검색 단어가 상품명이나 설명에 포함되었는가?
         if (StringUtils.hasText(condition.keyword())) {
             builder.and(product.name.containsIgnoreCase(condition.keyword())
                     .or(product.description.containsIgnoreCase(condition.keyword())));
         }
 
+        // 가격 특정 카테고리에 대한 검색인가?
+        // eq = equal
         if (condition.categoryId() != null) {
             builder.and(product.categoryId.eq(condition.categoryId()));
         }
 
+        // 최소가격이 정해졌는가?
+        // goe = greater or equal >=
         if (condition.minPrice() != null) {
             builder.and(product.price.goe(condition.minPrice()));
         }
 
+        // 최대 가격이 정해졌는가?
+        // loe = less or equal <=
         if (condition.maxPrice() != null) {
             builder.and(product.price.loe(condition.maxPrice()));
         }
 
+        // 판매자 아이디 필터링
         if (condition.sellerId() != null) {
             builder.and(product.sellerId.eq(condition.sellerId()));
         }
 
+        // 상태는 ACTIVE인 것만 검색
         builder.and(product.status.eq(ProductStatus.ACTIVE));
 
+        // builder로 만든 조건에 맞는 상품의 총 개수를 세는 쿼리
         // 전체 카운트 쿼리
         JPAQuery<Long> countQuery = queryFactory
                 .select(product.count())
@@ -73,7 +89,11 @@ public class ProductQueryRepositoryCustom implements ProductQueryRepository{
                         product.price,
                         product.stockQuantity,
                         // 카테고리 이름 대신 상수값을 변환
-                        Expressions.asString("Category").as("categoryName"),
+//                        Expressions.asString("Category").as("categoryName"),
+                        JPAExpressions
+                                .select(category.name)
+                                .from(category)
+                                .where(category.id.eq(product.categoryId)),
                         product.status))
                 .from(product)
                 .where(builder)
